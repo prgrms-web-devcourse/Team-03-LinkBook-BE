e11c494ce39bb81d088933a90fae89dd1254a377
suy2on <tnwjdrms@naver.com>
2022-07-22 18:13:46 +0900

    ✨init

---
+++ src/main/java/com/prgrms/team03linkbookbe/Team03LinkBookBeApplication.java
@@ -0,0 +1,14 @@
+package com.prgrms.team03linkbookbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class Team03LinkBookBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team03LinkBookBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prgrms/team03linkbookbe/Team03LinkBookBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.team03linkbookbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team03LinkBookBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
