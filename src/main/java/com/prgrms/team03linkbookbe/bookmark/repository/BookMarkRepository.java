53eabefdf6422e192eabe03cf19ae06f6c470319
suy2on <tnwjdrms@naver.com>
2022-08-03 03:12:55 +0900

    ✨테스트 의존성 추가

--- src/main/java/com/prgrms/team03linkbookbe/bookmark/repository/BookMarkRepository.java
+++
@@ -1,8 +0,0 @@
-package com.prgrms.team03linkbookbe.bookmark.repository;
-
-import com.prgrms.team03linkbookbe.bookmark.entity.Bookmark;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookMarkRepository extends JpaRepository<Bookmark, Long> {
-
-}
