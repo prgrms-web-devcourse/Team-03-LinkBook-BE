090404020a9ac371b66b4d549a1202ac0eeba8f1
ilhwan <poj0722@naver.com>
2022-08-10 01:45:05 +0900

    :recycle: 태그로 폴더 검색 리팩토링 (서브쿼리 사용)

--- src/main/java/com/prgrms/team03linkbookbe/folderTag/repository/FolderTagRepository.java
+++ src/main/java/com/prgrms/team03linkbookbe/folderTag/repository/FolderTagRepository.java
@@ -2,9 +2,7 @@ package com.prgrms.team03linkbookbe.folderTag.repository;
 
 import com.prgrms.team03linkbookbe.folder.entity.Folder;
 import com.prgrms.team03linkbookbe.folderTag.entity.FolderTag;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface FolderTagRepository extends JpaRepository<FolderTag, Long> {
 
@@ -16,7 +14,4 @@ public interface FolderTagRepository extends JpaRepository<FolderTag, Long> {
 //
 //    @Query("SELECT DISTINCT ft.folder FROM FolderTag ft JOIN FETCH ft.folder JOIN FETCH ft.tag WHERE ft.tag = :sub order by ft.folder.createdAt DESC ")
 //    Page<FolderTag> findFolderTagByRootTagOrderByCreatedAt(TagCategory sub);
-
-    @Query("SELECT DISTINCT ft FROM FolderTag ft JOIN FETCH ft.tag WHERE ft.folder.id = :folderId")
-    List<FolderTag> findByFolderId(Long folderId);
 }
