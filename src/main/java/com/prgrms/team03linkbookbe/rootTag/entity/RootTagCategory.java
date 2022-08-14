624b49498b2743e94e5ced76c3da704fc6b95640
Sumi Kim <sumikim323@naver.com>
2022-08-14 19:47:02 +0900

    [FEAT] Tag Enum 필드 추가 (#153)
    * :sparkles: 태그 카테고리 sub field 추가
    
    * :ambulance: 기존 dummy-tag rollback
    
    Co-authored-by: Sumi-gec <sumikim@gentleenergycorp.com>

--- src/main/java/com/prgrms/team03linkbookbe/rootTag/entity/RootTagCategory.java
+++ src/main/java/com/prgrms/team03linkbookbe/rootTag/entity/RootTagCategory.java
@@ -11,40 +11,99 @@ import lombok.Getter;
 public enum RootTagCategory {
     ALL("전체 카테고리", new TagCategory[0]),
     DAILY("일상", new TagCategory[]{
-        TagCategory.DAILY1
+        TagCategory.DAILY1,
+        TagCategory.HOBBY,
+        TagCategory.LIFEHACK
     }),
     ANIMAL("동물", new TagCategory[]{
-        TagCategory.ANIMAL1
+        TagCategory.ANIMAL1,
+        TagCategory.DOG,
+        TagCategory.CAT,
+        TagCategory.REPTILE,
+        TagCategory.INSECT
     }),
     GAME("게임", new TagCategory[]{
-        TagCategory.GAME1
+        TagCategory.GAME1,
+        TagCategory.FPS,
+        TagCategory.RPG,
+        TagCategory.TPS,
+        TagCategory.SPORTS_GAME,
+        TagCategory.RACING_GAME,
+        TagCategory.SIMULATION_GAME,
+        TagCategory.RHYTHM_GAME
     }),
     MOVIE("영화", new TagCategory[]{
-        TagCategory.MOVIE1
+        TagCategory.MOVIE1,
+        TagCategory.ROMANCE_MOVIE,
+        TagCategory.ACTION_MOVIE,
+        TagCategory.CRIME_MOVIE,
+        TagCategory.SF_MOVIE,
+        TagCategory.COMEDY_MOVIE,
+        TagCategory.THRILLER_MOVIE,
+        TagCategory.HORROR_MOVIE,
+        TagCategory.SPORTS_MOVIE,
+        TagCategory.FANTASY_MOVIE
     }),
     MUSIC("음악", new TagCategory[]{
-        TagCategory.MUSIC1
+        TagCategory.MUSIC1,
+        TagCategory.POP,
+        TagCategory.JPOP,
+        TagCategory.BALLADE,
+        TagCategory.RAP,
+        TagCategory.BAND,
+        TagCategory.ACOUSTIC,
+        TagCategory.JAZZ
     }),
     HUMOR("유머", new TagCategory[]{
-        TagCategory.HUMOR1
+        TagCategory.HUMOR1,
+        TagCategory.BLACK_COMEDY,
+        TagCategory.DAD_JOKE,
+        TagCategory.SATIRE
     }),
     HEALTH("헬스", new TagCategory[]{
-        TagCategory.HEALTH1
+        TagCategory.HEALTH1,
+        TagCategory.WORKOUT,
+        TagCategory.PILATES,
+        TagCategory.POWER_LIFTING,
+        TagCategory.CROSSFIT
     }),
     BEAUTY("뷰티", new TagCategory[]{
-        TagCategory.BEAUTY1
+        TagCategory.BEAUTY1,
+        TagCategory.MAKEUP,
+        TagCategory.CLOTHES
     }),
     SPORTS("스포츠", new TagCategory[]{
-        TagCategory.SPORTS1
+        TagCategory.SPORTS1,
+        TagCategory.SOCCER,
+        TagCategory.BASKETBALL,
+        TagCategory.BASEBALL,
+        TagCategory.BILLIARDS,
+        TagCategory.BOWLING,
+        TagCategory.VOLLEYBALL
     }),
     DEVELOP("개발", new TagCategory[]{
-        TagCategory.DEVELOP1
+        TagCategory.DEVELOP1,
+        TagCategory.FRONTEND,
+        TagCategory.BACKEND,
+        TagCategory.INFRA,
+        TagCategory.DEVOPS,
+        TagCategory.DATA_VISUALIZATION,
+        TagCategory.AI
     }),
     TRAVEL("여행", new TagCategory[]{
-        TagCategory.TRAVEL1
+        TagCategory.TRAVEL1,
+        TagCategory.DOMESTIC_TRAVEL,
+        TagCategory.OVERSEAS_TRAVEL,
+        TagCategory.BACKPACKING
     }),
     FOOD("음식", new TagCategory[]{
-        TagCategory.FOOD1
+        TagCategory.FOOD1,
+        TagCategory.KOREAN_FOOD,
+        TagCategory.JAPANESE_FOOD,
+        TagCategory.CHINESE_FOOD,
+        TagCategory.VIETNAMESE_FOOD,
+        TagCategory.WESTERN_FOOD,
+        TagCategory.DESSERT
     });
 
     private String viewName;
