741dad8436e54f93440863463b0555c6fbe3477e
nnagman <nnagman@gmail.com>
2022-08-15 18:00:40 +0900

    :bug:좋아요 폴더 연관관계 버그 수정

--- src/main/java/com/prgrms/team03linkbookbe/like/entity/Like.java
+++ src/main/java/com/prgrms/team03linkbookbe/like/entity/Like.java
@@ -2,13 +2,20 @@ package com.prgrms.team03linkbookbe.like.entity;
 
 import com.prgrms.team03linkbookbe.folder.entity.Folder;
 import com.prgrms.team03linkbookbe.user.entity.User;
+import java.util.Objects;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
 @Getter
@@ -20,7 +27,7 @@ public class Like {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     @JoinColumn(name = "folder_id", referencedColumnName = "id")
     private Folder folder;
 
@@ -37,10 +44,15 @@ public class Like {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Like like = (Like) o;
-        return Objects.equals(id, like.id) && Objects.equals(folder, like.folder) && Objects.equals(user, like.user);
+        return Objects.equals(id, like.id) && Objects.equals(folder, like.folder) && Objects.equals(
+            user, like.user);
     }
 
     @Override
