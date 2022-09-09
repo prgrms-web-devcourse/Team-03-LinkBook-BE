83186e720a734682520913f272c75634d4d65f9d
suy2on <tnwjdrms@naver.com>
2022-09-09 23:17:52 +0900

    ✨폴더의 기존 북마크 가져오는 쿼리추가

--- src/main/java/com/prgrms/team03linkbookbe/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/prgrms/team03linkbookbe/bookmark/repository/BookmarkRepository.java
@@ -2,9 +2,13 @@ package com.prgrms.team03linkbookbe.bookmark.repository;
 
 import com.prgrms.team03linkbookbe.bookmark.entity.Bookmark;
 import com.prgrms.team03linkbookbe.folder.entity.Folder;
+import java.awt.print.Book;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
-    void deleteAllByFolder(Folder folder);
+    @Query("select b from Bookmark b where b.folder = :folder")
+    List<Bookmark> findAllByFolder(Folder folder);
 }
